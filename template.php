0d3da4cfa549a4d988c248803f0a3f7301fa26e2
laras126 <laras126@gmail.com>
2015-04-10 17:18:10 -0400

    add template.php, json

---
+++ template.php
@@ -0,0 +1,11 @@
+<?php 
+	$data_file_name = "template.json";
+	include('partials/head.php'); 
+?>
+  
+    <h1>{{page_title}}</h1>
+    <p>{{page_lead}}</p>
+
+    <p>{{text_content}}</p>
+  
+<?php include('partials/base.php'); ?>
